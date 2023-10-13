63522c958edd9f7401719334b19d3a8bf761b205
glooneltharion <gloo.neltharion@gmail.com>
2023-10-13 09:24:45 +0200

    inco 13.10.2023

--- module02/inco07/inco07.py
+++ module02/inco07/inco07.py
@@ -74,3 +74,54 @@ print(match_star)
 patter_plus = r'c+'
 match_plus = re.findall(patter_plus, text)
 print(match_plus)
+
+
+def process_text_file(input_file_path, output_file_path):
+    email_addresses = []
+    phone_numbers = []
+    dates = []
+    urls = []
+    ip_addresses = []
+    html_tags = []
+
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    phone_pattern = r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}'
+    date_pattern = r'(\d{2}/\d{2}/\d{4})'
+    url_pattern = r'https?://\S+|www\.\S+'
+    ip_pattern = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+    html_tag_pattern = r'<[^>]*>'
+
+    with open(input_file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+        email_addresses = re.findall(email_pattern, content)
+        phone_numbers = re.findall(phone_pattern, content)
+        dates = re.findall(date_pattern, content)
+        urls = re.findall(url_pattern, content)
+        ip_addresses = re.findall(ip_pattern, content)
+        html_tags = re.findall(html_tag_pattern, content)
+
+    dates = [date.replace('/', '-') for date in dates]
+
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        output_file.write("Email Addresses:\n")
+        output_file.write("\n".join(email_addresses) + "\n\n")
+
+        output_file.write("Phone Numbers:\n")
+        output_file.write("\n".join(phone_numbers) + "\n\n")
+
+        output_file.write("Dates:\n")
+        output_file.write("\n".join(dates) + "\n\n")
+
+        output_file.write("URLs:\n")
+        output_file.write("\n".join(urls) + "\n\n")
+
+        output_file.write("IP Addresses:\n")
+        output_file.write("\n".join(ip_addresses) + "\n\n")
+
+        output_file.write("HTML Tags:\n")
+        output_file.write("\n".join(html_tags) + "\n\n")
+
+
+# Example usage:
+process_text_file("input.txt", "output.txt")
---
+++ module02/inco08.py
@@ -0,0 +1,59 @@
+import sys
+import os
+import subprocess
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+print("Area: " + str(area))
+
+home = os.environ.get("HOME")
+print("HOME: ", home)
+
+user1 = os.environ.get("USER")
+print("User1: ", user1)
+
+os.environ["NEW_USER"] = "Samuel"
+user2 = os.environ.get("NEW_USER")
+print("User2: ", user2)
+
+if len(sys.argv) != 2:
+    print("Usage: python inco08.py action")
+    sys.exit(1)
+
+action = sys.argv[1]
+
+if action == "calculate":
+    result = 8 * 4
+    print(result)
+    sys.exit(0)
+elif action == "exit":
+    print("Exiting program")
+    sys.exit(0)
+else:
+    print("Invalid action.")
+    sys.exit(1)
+
+gitbash = "C:\\Program Files\\Git\\bin\\bash.exe"
+#gitbash = os.environ.get("GITBASH")
+try:
+    completed_process = subprocess.run([gitbash, "-c", "ls"], capture_output=True, text=True, shell=True, check=True)
+    #completed_process = subprocess.run(["dir"], capture_output=True, text=True, shell=True, check=True)
+    output = completed_process.stdout
+    print(output)
+    exit_code = completed_process.returncode
+
+    if exit_code == 0:
+        print("Success")
+    else:
+        print("Fail ", exit_code)
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
+
+
+
+
+
+
+
+
